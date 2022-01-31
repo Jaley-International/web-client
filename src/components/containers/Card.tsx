d566842b905a840143cdfaa2a4e66e3a029ea1de
Yohann THEPAUT <contact@ythepaut.com>
2022-01-31 13:09:02 +0000

    Add complete user details view from admin

--- src/components/containers/Card.tsx
+++ src/components/containers/Card.tsx
@@ -3,6 +3,7 @@ import {Heading5} from "../text/Headings";
 interface Props {
     children: JSX.Element;
     title?: string;
+    separator?: boolean;
     className?: string;
 }
 
@@ -11,7 +12,7 @@ function Card(props: Props): JSX.Element {
         <div className={`bg-bg shadow-card rounded-2xl ${props.className ? props.className : ""}`}>
             <>
                 {props.title &&
-                    <div className="w-full px-6 border-b border-grey-200">
+                    <div className={`w-full px-6 ${props.separator === true || props.separator === undefined ? "border-b border-grey-200" : ""}`}>
                         <Heading5 className="py-4">{props.title}</Heading5>
                     </div>
                 }
