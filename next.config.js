67239d1a73eb971967a4b889aa77f1b96a478a24
Lucas <lucas.chardonnet@etu.univ-smb.fr>
2022-02-23 17:20:03 +0000

    Add translation system and French translation for login page

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
     reactStrictMode: true,
     publicRuntimeConfig: {
         apiUrl: process.env.PEC_CLIENT_API_URL
+    },
+    i18n: {
+        locales: ["en", "fr"],
+        defaultLocale: "en"
     }
 };
